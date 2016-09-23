89e98ae59697be4e046065f36d1eb0d7a2a5bcc8
egorkam <egorkam_lp@ukr.net>
2016-09-24 01:47:57 +0300

    d

---
+++ NodejsWebApp1/NodejsWebApp1/server.js
@@ -0,0 +1,10 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    var random_number = getRandomArbitary(1, 10).toString()
+    res.writeHead(200, { 'Content-Type': 'text/plain' });1
+    res.end('_testcb(\"' + random_number + '\")');
+}).listen(port);
+function getRandomArbitary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
